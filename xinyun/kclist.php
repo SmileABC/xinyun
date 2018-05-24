d5dbc44e3bb6ca939b36777c44b40ac900d86161
SmileABC <164439926@qq.com>
2018-05-24 16:06:48 +0800

    修改

--- xinyun/kclist.php
+++ xinyun/kclist.php
@@ -2,11 +2,6 @@
 include_once 'db.php';
 $page = $_GET['page'];
 $kctype = $_GET['kctype'];
-//if($page){
-//}else{
-//    $page = 0;
-//    SELECT * FROM kecheng_list where kctype=1 LIMIT 0,5;
-//}
 $dsn = 'mysql:host='.db_host.';port='.db_port.';dbname='.db_database;
 $pdo = new PDO($dsn, db_user, db_pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"));
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);   //attr_errmode  errmode_exception
