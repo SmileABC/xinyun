f201648c21fd9fa975298968c5dcfce32160e31f
SmileABC <164439926@qq.com>
2018-05-28 16:03:34 +0800

    笔记

--- xinyun/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('db_host','qdm136487289.my3w.com');
-define('db_user','qdm136487289');
-define('db_pwd','164439926');
-define('db_database','qdm136487289_db');
-define('db_port',3306);
